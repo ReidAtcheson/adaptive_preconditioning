697c1716c7eaf84b5d477e14013fe052a9cb5c3c
Reid Atcheson <reid.atcheson@gmail.com>
2022-07-16 13:11:57 -0500

    solver and precon added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse as sp
+import scipy.sparse.linalg as spla
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -23,10 +24,60 @@ def make_banded_matrix(m,diag,bands,rng):
     offs = [-x for x in bands] + [0] + [x for x in bands]
     return sp.diags(subdiags,offs,shape=(m,m))
 
+def make_block_precon(A,blocksize):
+    A=sp.lil_matrix(A)
+    m,_=A.shape
+    blocks=[]
+    for i in range(0,m,blocksize):
+        beg=i
+        end=min(i+blocksize,m)
+        ids=list(range(beg,end))
+        blocks.append(A[np.ix_(ids,ids)])
+    return sp.block_diag(blocks)
+
+
+
+def plain_precon_richardson(A,b,P,reltol=1e-8):
+    m,_=A.shape
+
+    x=np.zeros(m)
+    r=b-A@x
+    it=0
+    while np.linalg.norm(r)/np.linalg.norm(b) > reltol:
+        it=it+1
+        #Precondition the residual
+        z = P(r)
+        #Find optimal step size
+        Az = A@z
+        tau = np.dot(r,Az)/np.dot(Az,Az)
+        #Updates
+        x = x + tau*z
+        r = r - tau*Az
+        print(f"it : {it}, res = {np.linalg.norm(r)}")
+    return x
+
+
+
+
 
 
 seed=23498732
 rng=np.random.default_rng(seed)
-A=make_banded_matrix(512,4,[1,2,3,10,40,100],rng)
-plt.spy(A,markersize=1)
+m=512
+diag=4
+blocksize=32
+
+
+A=make_banded_matrix(m,diag,[1,2,3,10,40,100],rng)
+Ab=make_block_precon(A,blocksize)
+luAb=spla.splu(sp.csc_matrix(Ab))
+
+
+plt.spy(Ab,markersize=1)
 plt.savefig("spyA.svg")
+
+b=np.ones(m)
+plain_precon_richardson(A,b,lambda x : luAb.solve(x))
+
+
+
